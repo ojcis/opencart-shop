272efccaaa9fb474fb778d1f2dd5d29d3966b793
Ojārs <ojars.meldris@gmail.com>
2023-02-20 19:27:01 +0200

    add to git

---
+++ README.md
@@ -0,0 +1,16 @@
+# OpenCart-shop
+
+### Category filter
+### Discount label
+### Order info
+
+## Technologies used:
+* PHP 7.4
+* mySQL 8.0
+* Composer 2.4
+
+## Instructions
+* Clone this repository.
+* Install the required packages using the command:<br><code>composer install</code>
+* Start the local development server by using the command:<br><code>php -S localhost:8000</code>
+* 
---
+++ orderInfo.php
@@ -0,0 +1,45 @@
+<?php
+const OPENCART_URL = 'http://localhost:8000';
+$apiKey = readline('API key: ');
+$orderId = readline('Order ID: ');
+
+session_start();
+$_COOKIE['PHPSESSID'] = session_id();
+session_write_close();
+
+$url = OPENCART_URL."/index.php?route=api/login";
+$postBody = ["key=$apiKey"];
+
+$response=getResponse($url, $postBody);
+$token = $response->token;
+$url = OPENCART_URL.'/index.php?route=api/order/info&token='.$token.'&order_id='.$orderId;
+$response = getResponse($url);
+
+//var_dump($response);
+echo PHP_EOL;
+echo "order id: {$response->order->order_id}".PHP_EOL;
+echo "customer name: {$response->order->firstname} {$response->order->lastname}".PHP_EOL;
+echo "customer email: {$response->order->email}".PHP_EOL;
+echo "order status: {$response->order->order_status}".PHP_EOL;
+echo "order price: {$response->order->total}{$response->order->currency_code}".PHP_EOL;
+echo "payment method: {$response->order->payment_method}".PHP_EOL;
+echo "shipping address:{$response->order->shipping_country}, {$response->order->shipping_zone}, {$response->order->shipping_address_1}, {$response->order->shipping_address_2}, {$response->order->shipping_postcode}".PHP_EOL;
+
+function getResponse(string $url, array $postBody = []){
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => implode('&', $postBody),
+        CURLOPT_COOKIE => 'PHPSESSID=' . $_COOKIE['PHPSESSID'] . '; path=/',
+        CURLOPT_HTTPHEADER => [
+            "cache-control: no-cache",
+            "content-type: application/x-www-form-urlencoded",
+        ],
+    ]);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($response);
+}
+
