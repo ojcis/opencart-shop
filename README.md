0a120126a4fb16a578475238e96e813ab80aec26
Ojārs <ojars.meldris@gmail.com>
2023-02-20 19:45:15 +0200

    update

--- README.md
+++ README.md
@@ -12,5 +12,8 @@
 ## Instructions
 * Clone this repository.
 * Install the required packages using the command:<br><code>composer install</code>
-* Start the local development server by using the command:<br><code>php -S localhost:8000</code>
-* 
+* Rename <code>config-dist.php</code> to <code>config.php</code> in <code>upload</code> directory an <code>upload/admin</code> directory.
+* Start the local development server by using the command in <code>upload</code> directory:<br><code>php -S localhost:8000</code>
+* Install OpenCart (http://localhost:8000/)
+* As the admin generate an API key in System -> Users -> API.
+* To get order info open new terminal, navigate to <code>opencart-shop</code> directory and run comand:<br><code>php orderInfo.php</code>.
---
+++ upload/admin/config-dist.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:8000/admin/');
+define('HTTP_CATALOG', 'http://localhost:8000/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:8000/admin/');
+define('HTTPS_CATALOG', 'http://localhost:8000/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ojars/Documents/GitHub/opencart-shop/upload/admin/');
+define('DIR_SYSTEM', '/home/ojars/Documents/GitHub/opencart-shop/upload/system/');
+define('DIR_IMAGE', '/home/ojars/Documents/GitHub/opencart-shop/upload/image/');
+define('DIR_LANGUAGE', '/home/ojars/Documents/GitHub/opencart-shop/upload/admin/language/');
+define('DIR_TEMPLATE', '/home/ojars/Documents/GitHub/opencart-shop/upload/admin/view/template/');
+define('DIR_CONFIG', '/home/ojars/Documents/GitHub/opencart-shop/upload/system/config/');
+define('DIR_CACHE', '/home/ojars/Documents/GitHub/opencart-shop/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ojars/Documents/GitHub/opencart-shop/upload/system/storage/download/');
+define('DIR_LOGS', '/home/ojars/Documents/GitHub/opencart-shop/upload/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ojars/Documents/GitHub/opencart-shop/upload/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ojars/Documents/GitHub/opencart-shop/upload/system/storage/upload/');
+define('DIR_CATALOG', '/home/ojars/Documents/GitHub/opencart-shop/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ojars');
+define('DB_PASSWORD', 'ojars1997');
+define('DB_DATABASE', 'opencart-shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
